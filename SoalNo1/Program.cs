5613f17cff56384839394e532d943383796e26a4
Lendra Kusuma Wardoyo <lendrakusuma.w@gmail.com>
2019-09-25 02:53:18 +0700

    Program No 1 & 4

---
+++ SoalNo1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoalNo1
+{
+    class Program
+    {
+        static List<string> lstPenggalanKata;
+        static void Main(string[] args)
+        {
+            lstPenggalanKata = new List<string>();
+
+            string initialStopInsertPenggalan = "";
+
+            do
+            {
+                Console.Write("Inputkan penggalan kata, ketik tanda pagar (#) jika selesai:");
+                initialStopInsertPenggalan = Console.ReadLine();
+
+                if (initialStopInsertPenggalan != "#")
+                {
+                    lstPenggalanKata.Add(initialStopInsertPenggalan);
+                    initialStopInsertPenggalan = "";
+                }
+
+            } while (initialStopInsertPenggalan.ToUpper() == "");
+
+            string kata = "";
+
+            do
+            {
+                Console.Write("Inputkan kata yg akan dipenggal, jika tidak inputkan tanda pagar (#):");
+                kata = Console.ReadLine();
+
+                if (kata != "#")
+                {
+                    for (int i = 0; i < lstPenggalanKata.Count; i++)
+                    {
+                        kata = kata.ToLower();
+                        if (kata.StartsWith(lstPenggalanKata[i].ToLower()))
+                        {
+                            List<string> lstPenggalan = new List<string> { lstPenggalanKata[i].ToLower() };
+
+                            PemenggalanKata(kata, ref lstPenggalan);
+
+                            Console.WriteLine(String.Join(",", lstPenggalan));
+                        }
+                    }
+                    kata = "";
+                }
+
+            } while (kata == "");
+
+        }
+
+        private static void PemenggalanKata(string kata, ref List<string> lstPenggalan)
+        {
+            string sisaKata = kata.Substring(String.Join("", lstPenggalan).Length);
+
+            for (int i = 0; i < lstPenggalanKata.Count; i++)
+            {
+                sisaKata = sisaKata.ToLower();
+                if (sisaKata.StartsWith(lstPenggalanKata[i].ToLower()))
+                {
+                    lstPenggalan.Add(lstPenggalanKata[i].ToLower());
+
+                    PemenggalanKata(kata, ref lstPenggalan);
+                }
+            }
+        }
+    }
+}
---
+++ SoalNo4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoalNo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan harga saham setiap hari dibatasi dgn (,):");
+            string hargaSaham = Console.ReadLine();
+
+            string[] arrHargaSaham = hargaSaham.Split(',');
+
+            Console.WriteLine(String.Join(",", arrHargaSaham));
+
+            int profit = 0;
+            int hargaBeliTerpilih = 0;
+            int hargaJualTerpilih = 0;
+
+            for (int i = 0; i < arrHargaSaham.Length; i++)
+            {
+                int hargaBeli = int.Parse(arrHargaSaham[i]);
+                for (int j = i + 1; j < arrHargaSaham.Length; j++)
+                {
+                    int hargaJual = int.Parse(arrHargaSaham[j]);
+                    if (hargaJual - hargaBeli > profit)
+                    {
+                        profit = hargaJual - hargaBeli;
+
+                        hargaBeliTerpilih = hargaBeli;
+                        hargaJualTerpilih = hargaJual;
+                    }
+                }
+            }
+
+            if (profit == 0)
+            {
+                Console.WriteLine("<tidak dapat membeli>");
+            }
+            else
+            {
+                Console.WriteLine("Harga Beli:" + hargaBeliTerpilih + "; Harga Jual:" + hargaJualTerpilih + "; Profit:" + profit);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
